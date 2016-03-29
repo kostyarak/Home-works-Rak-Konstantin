27290b042a5cfba7b3c461052077261e62a69d06
kostyarak <kot22_85@mail.ru>
2016-03-29 19:18:29 +0300

    add tag Form

--- hw9/scripthw9.js
+++ hw9/scripthw9.js
@@ -1,4 +1,3 @@
-
 var body = document.getElementsByTagName('body');
 var newElement = document.createElement('div');
 newElement.className = "wrapper";
@@ -12,26 +11,26 @@ document.body.appendChild(newElement);
 	wrapper.appendChild(newElement);
 
 		// var wrapper = document.body.querySelector('.wrapper');
-		newElement.className = "test";
+			newElement=document.createElement('form');
         
 		newElement.setAttribute('name', 'test');
-    	newElement.setAttribute('method', 'GET');
- 	    newElement.setAttribute('action', '');
+    		newElement.setAttribute('method', 'GET');
+ 	    	newElement.setAttribute('action', '');
 		wrapper.appendChild(newElement);
 
-		var test = wrapper.querySelector('.test');
-		for(var i=1; i<4; i++){
+		  var form = wrapper.querySelector('form');
+		  for(var i=1; i<4; i++){
 		  newElement = document.createElement('p');
 		  newElement.innerHTML = i+'. Вопрос №'+i;
 		  newElement.style.marginLeft='5px';
 		  newElement.style.marginTop ='30px';
 
-		  test.appendChild(newElement);
+		  form.appendChild(newElement);
 
 			  newElement = document.createElement('ul');
-			  test.appendChild(newElement);
+			  form.appendChild(newElement);
 
-			  var ul=test.querySelectorAll('ul');
+			  var ul=form.querySelectorAll('ul');
 			  for(var n=1; n<4; n++){
 			  newElement=document.createElement('li');
 			  newElement.style.listStyle='none';
@@ -40,8 +39,8 @@ document.body.appendChild(newElement);
 				 var li = ul[i-1].querySelectorAll('li');
 				 newElement = document.createElement('input');
 				 newElement.setAttribute('type', 'checkbox');
- 	             newElement.setAttribute('name', 'an_'+i+'-'+n);
- 	             newElement.setAttribute('id', 'an_'+i+'-'+n);
+ 	             		 newElement.setAttribute('name', 'an_'+i+'-'+n);
+ 	                         newElement.setAttribute('id', 'an_'+i+'-'+n);
 				 li[n-1].appendChild(newElement);
 
 				  newElement = document.createElement('label');
@@ -59,4 +58,4 @@ newElement.innerHTML = 'Проверить мои результаты';
 newElement.style.marginTop = '20px';
 newElement.style.marginLeft='5px';
 newElement.setAttribute('type', 'submit');
-test.appendChild(newElement);
+form.appendChild(newElement);
